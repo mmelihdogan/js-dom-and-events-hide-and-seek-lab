9cd752e86378c0ed9040f58a19ff875e61fcef7f
mmelihdogan <doganmmelih@gmail.com>
2020-01-21 19:40:59 +0300

    finished the assignments

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+    let val = document.querySelector(selector);
+    return val;
+}
+
+function nestedTarget() {
+    // return document.getElementById('nested').getElementsByClassName('target')[0];
+    // return document.getElementById('nested').getSelectorAll('div.target')[0];
+    return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+    let element = document.querySelector('#grand-node');
+
+    return element.querySelectorAll('div')[3];
+}
+
+function increaseRankBy(n) {
+    let rankedList = document.querySelectorAll('.ranked-list li');
+
+    for (let index = 0; index < rankedList.length; index++) {
+        const element1 = rankedList[index];
+        let nums = element1.textContent;
+        let number = parseInt(nums);
+        number = number + n;
+        element1.textContent = number;
+    }
+}
